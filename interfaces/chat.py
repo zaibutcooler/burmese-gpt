f81c43b22716269935cd908708b72d6fa50b8bf1
Zai <130903099+zaiYellYintAung@users.noreply.github.com>
2023-11-11 23:38:33 +0630

    added chat and stream interface

---
+++ interfaces/chat.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("Burmese GPT")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
